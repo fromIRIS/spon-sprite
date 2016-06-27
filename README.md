0fbe297eb5761cd534284d546322dc91fb66483d
南洋 <417611525@qq.com>
2016-06-27 16:26:09 +0800

    docs(readme): 文档添加

---
+++ README.md
@@ -0,0 +1,37 @@
+## 简介
+
+`spon-sprite`
+
+将图片生成雪碧图，压缩，上传至showjoyCDN, 并生成对应的css文件。
+
+## 使用方法
+
+### 进入需要sprite化的图片文件夹
+
+### 命令
+
+1.H5
+
+```
+spon sprite -m
+```
+
+2.pc
+
+```
+spon sprite
+```
+
+### 生成
+
+```
+[sprite-image]
+  | -- kulian_03.png
+  | -- min_10.png
+  | -- people-plus_03.png
+  | -- people_03.png
+  | -- plus_06.png
+  | -- [sprite]
+  |     | -- sprite.css
+  |     | -- sprite.png
+```
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var path = require('path');
 var spritesmith = require('spritesmith');
 var fs = require('fs');
+var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var notify = require('gulp-notify');
@@ -26,114 +27,136 @@ var distPath   = {
     };
 
 
-module.exports = function (config) {
+module.exports = function (spon) {
 
-    var _isH5 = config.mobi;
-    var outputPath = './src/images';
+    /**
+     * sprite
+     */
+    spon
+    .command('sprite [cmd]')
+    .description('传统前端开发方案')
+    .option("-m, --mobi [type]", "是否H5环境")
+    .action(function(cmd, config){
+      
+      spriteTask(config);
 
-    var pngPathArr = (function () {
-      var imgArr = fs.readdirSync(srcPath.SPRITE);
-    
-      var pngArr = imgArr.filter(function (item, i) {
-        return item.search(/\.png/gi) > -1;
-      })
-
-      var pngPathArr = pngArr.map(function (item, i) {
-        return path.join(srcPath.SPRITE, item);
-      })
-
-      return pngPathArr;
-    })();
-
-    function postSpritePng() {
-      return new Promise(function (resolve, reject) {
-        log.info('start uploading sprite png')
-        var formData = {
-
-          file: fs.createReadStream(path.join(outputPath, 'sprite.png')),
-          
-        };
-        request.post({url:'http://file.showjoy.com/saveFile/', formData: formData}, function optionalCallback(err, httpResponse, body) {
-          if (err) {
-            reject(new Error(err));
-          } else {
-            
-            resolve(body);
-          }
-        });
-
-      })
-    }
-    
-
-    //压缩图片
-    gulp.task('imagemin', function () {
-       return gulp.src(path.join(outputPath, 'sprite.png'))
-        .pipe(notify({message: 'starting imagemin!'}))
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()] //深度png压缩
-        }))
-        .pipe(gulp.dest(srcPath.IMG))
-        .pipe(notify({message: 'finished imagemin!'}));
     });
 
-    var getCssString = function (coor, prop, imgPath) {
-      var codeStr = '';
-      for (key in coor) {
-        var code = '"' + key + '" ' + '{' + '\n';
-            code += 'background: ';
-            code += 'url(' + imgPath + ') ';
-            code += 'no-repeat;';
-            code += '\n';
-            code += 'background-position: -';
-            code += (_isH5 !== undefined) ? coor[key].x / 2 : coor[key].x;
-            code += 'px -';
-            code += _isH5 ? coor[key].y / 2 : coor[key].y;
-            code += 'px;';
-            code += '\n';
-            code += 'background-size: ';
-            code += _isH5 ? prop.width / 2 : prop.width;
-            code += 'px ';
-            code += _isH5 ? prop.height / 2 : prop.height;
-            code += 'px;';
-            code += '\n';
-            code += 'width: ';
-            code += _isH5 ? coor[key].width / 2 : coor[key].width;
-            code += 'px;';
-            code += '\n';
-            code += 'height: ';
-            code += _isH5 ? coor[key].height / 2 : coor[key].height;
-            code += 'px;';
-            code += '\n';
-            code += '}';
-            code += '\n';
-            codeStr += code;
-      }
-      return codeStr;
-    }
+    function spriteTask (config) {
+      var _isH5 = config.mobi;
+      var outputPath = path.join(process.cwd(), 'sprite');
 
-    var makeSprite = function (cb) {
-      spritesmith({src: pngPathArr}, function handleResult (err, result) {
-
-        fs.writeFileSync(path.join(outputPath, 'sprite.png'), result.image);
-        gulp.start('imagemin', function () {
-
-          postSpritePng().then(function (body) {
-            log.info('Upload successful!');
-            var dataJson = JSON.parse(body);
-            var cssstr = getCssString(result.coordinates, result.properties, dataJson.data.replace('file', 'cdn1'));
-            cb(cssstr)
-          }).catch(function (err) {
-            log.error(err);
-          })
-        });
+      fs.exists(outputPath, function (exists) {
+        if (!exists) {
+          return fs.mkdirSync(outputPath);
+        }
       });
-    }
 
-    makeSprite(function (cssstr) {
-      // 将css写入样式文件
-      fs.writeFileSync(path.join(srcPath.SPRITE, 'sprite.css'), cssstr);
-    })
+      var pngPathArr = (function () {
+        var imgArr = fs.readdirSync(process.cwd());
+      
+        var pngArr = imgArr.filter(function (item, i) {
+          return item.search(/\.png/gi) > -1;
+        })
+
+        var pngPathArr = pngArr.map(function (item, i) {
+          return path.join(process.cwd(), item);
+        })
+
+        return pngPathArr;
+      })();
+
+      function postSpritePng() {
+        return new Promise(function (resolve, reject) {
+          log.info('start uploading sprite png')
+          var formData = {
+
+            file: fs.createReadStream(path.join(outputPath, 'sprite.png')),
+            
+          };
+          request.post({url:'http://file.showjoy.com/saveFile/', formData: formData}, function optionalCallback(err, httpResponse, body) {
+            if (err) {
+              reject(new Error(err));
+            } else {
+              
+              resolve(body);
+            }
+          });
+
+        })
+      }
+      
+
+      //压缩图片
+      gulp.task('imagemin', function () {
+         return gulp.src(path.join(outputPath, 'sprite.png'))
+          .pipe(notify({message: 'starting imagemin!'}))
+          .pipe(imagemin({
+              progressive: true,
+              svgoPlugins: [{removeViewBox: false}],
+              use: [pngquant()] //深度png压缩
+          }))
+          .pipe(gulp.dest(outputPath))
+          .pipe(notify({message: 'finished imagemin!'}));
+      });
+
+      var getCssString = function (coor, prop, imgPath) {
+        var codeStr = '';
+        for (key in coor) {
+          var code = '"' + key + '" ' + '{' + '\n';
+              code += 'background: ';
+              code += 'url(' + imgPath + ') ';
+              code += 'no-repeat;';
+              code += '\n';
+              code += 'background-position: -';
+              code += (_isH5 !== undefined) ? coor[key].x / 2 : coor[key].x;
+              code += 'px -';
+              code += _isH5 ? coor[key].y / 2 : coor[key].y;
+              code += 'px;';
+              code += '\n';
+              code += 'background-size: ';
+              code += _isH5 ? prop.width / 2 : prop.width;
+              code += 'px ';
+              code += _isH5 ? prop.height / 2 : prop.height;
+              code += 'px;';
+              code += '\n';
+              code += 'width: ';
+              code += _isH5 ? coor[key].width / 2 : coor[key].width;
+              code += 'px;';
+              code += '\n';
+              code += 'height: ';
+              code += _isH5 ? coor[key].height / 2 : coor[key].height;
+              code += 'px;';
+              code += '\n';
+              code += '}';
+              code += '\n';
+              codeStr += code;
+        }
+        return codeStr;
+      }
+
+      var makeSprite = function (cb) {
+        spritesmith({src: pngPathArr}, function handleResult (err, result) {
+
+          fs.writeFileSync(path.join(outputPath, 'sprite.png'), result.image);
+          gulp.start('imagemin', function () {
+
+            postSpritePng().then(function (body) {
+              log.info('Upload successful!');
+              var dataJson = JSON.parse(body);
+              var cssstr = getCssString(result.coordinates, result.properties, dataJson.data.replace('file', 'cdn1'));
+              cb(cssstr)
+            }).catch(function (err) {
+              log.error(err);
+            })
+          });
+        });
+      }
+
+      // 最终调用makeSprite
+      makeSprite(function (cssstr) {
+        // 将css写入样式文件
+        fs.writeFileSync(path.join(outputPath, 'sprite.css'), cssstr);
+      })
+    }
 } 
